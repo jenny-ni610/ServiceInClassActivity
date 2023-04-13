ed1f1edebf797576cb11efe42781237bcaaa59ee
Jenny Ni <tun48961@temple.edu>
2023-04-13 12:02:44 -0400

    bind service in main activity and using the bind service to start, pause, and stop the timer

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -3,27 +3,50 @@ package edu.temple.myapplication
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    /** Defines callbacks for service binding, passed to bindService().  */
+    val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.playButton).setOnClickListener {
+        //startService(Intent(this, TimerService::class.java))
+        bindService(Intent(this, TimerService::class.java), connection, BIND_AUTO_CREATE)
 
+        findViewById<Button>(R.id.playButton).setOnClickListener {
+            if(isConnected) timerBinder.start(100)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if(isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if(isConnected) timerBinder.stop()
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(connection)
+        super.onDestroy()
     }
 }
